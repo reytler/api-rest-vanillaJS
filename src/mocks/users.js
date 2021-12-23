9524e906d3488a76e9d1cb1c52cf18ad05387f95
Reytler Souza <reytler@pmedico.com>
2021-12-23 14:42:35 -0300

    Criação da API REST

---
+++ src/mocks/users.js
@@ -0,0 +1,14 @@
+module.exports = [
+    {
+        id: 1,
+        name: 'Reytler',
+    },
+    {
+        id: 2,
+        name: 'Matheus',
+    },
+    {
+        id: 3,
+        name: 'Souza',
+    }
+]
